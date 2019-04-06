bef39a4d89034953b8e157b55b8f70547ff84546
bladerunner212 <nickita.pateeff@ya.ru>
2019-04-06 15:16:51 +0300

    bug with routing fixed

--- frontend/src/app/modules/header/models/mock-links.ts
+++ frontend/src/app/modules/header/models/mock-links.ts
@@ -1,11 +1,9 @@
 import {Link} from "./link"
 
 export const LINKS: Link[] = [
-
       {id: 1, name: 'Home'},
-	  {id: 2, name: 'Music'},
+	    {id: 2, name: 'Music'},
       {id: 3, name: 'Magazines'},
       {id: 4, name: 'Netflix'},
-      {id: 5, name: 'Sign in/Sign up'},
-	  {id: 6, name: 'User'}
+	    {id: 5, name: 'Account'}
 	];
